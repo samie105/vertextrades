973b5841f3482245b4715f30fb12e497ed62bfbd
samie105 <samsonrichfield@gmail.com>
2023-07-13 21:47:54 +0100

    updated testimonials

--- components/main/Testimonials/Testimonials.js
+++ components/main/Testimonials/Testimonials.js
@@ -2,7 +2,11 @@
 "use client";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faReddit,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import Slider from "react-slick";
@@ -106,9 +110,10 @@ export default function Testimonials() {
                       className="w-3 h-3 inline text-gray-300"
                     />
                   </sup>{" "}
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Veniam, deserunt. Voluptate veniam facere, delectus eveniet
-                  nisi quis incidunt mollitia veritatis.{" "}
+                  I can't thank my broker enough for their outstanding service.
+                  From the moment I signed up, they have provided exceptional
+                  support and guidance. Their platform is intuitive and packed
+                  with powerful and experiences
                   <sup>
                     <FontAwesomeIcon
                       icon={faQuoteRight}
@@ -154,9 +159,10 @@ export default function Testimonials() {
                       className="w-3 h-3 inline text-gray-300"
                     />
                   </sup>{" "}
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Veniam, deserunt. Voluptate veniam facere, delectus eveniet
-                  nisi quis incidunt mollitia veritatis.{" "}
+                  Choosing this broker was a game-changer for me. Their
+                  dedication to customer satisfaction is unmatched. Whenever I
+                  have a question or need assistance, their support team is
+                  always there to help.{" "}
                   <sup>
                     <FontAwesomeIcon
                       icon={faQuoteRight}
@@ -172,7 +178,7 @@ export default function Testimonials() {
                       height={50}
                       width={50}
                       className=""
-                      src="/assets/nellyaran.jpg"
+                      src="/assets/testiii.jpg"
                       alt=""
                     />
                   </div>
@@ -189,54 +195,6 @@ export default function Testimonials() {
               <div className="card-brand mb-4">
                 <div className="icon-container flex items-center font-bold ">
                   <div className=" mr-2">
-                    <FontAwesomeIcon icon={faTwitter} className="w-4 h-4" />
-                  </div>
-                  <p>Twitter</p>
-                </div>
-              </div>
-              <div className="card-body text-sm">
-                <blockquote>
-                  <sup>
-                    <FontAwesomeIcon
-                      icon={faQuoteLeft}
-                      className="w-3 h-3 inline text-gray-300"
-                    />
-                  </sup>{" "}
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Veniam, deserunt. Voluptate veniam facere, delectus eveniet
-                  nisi quis incidunt mollitia veritatis.{" "}
-                  <sup>
-                    <FontAwesomeIcon
-                      icon={faQuoteRight}
-                      className="w-3 h-3 inline text-gray-300"
-                    />
-                  </sup>
-                </blockquote>
-              </div>
-              <div className="card-author mt-5 flex items-center">
-                <div className="avatar ">
-                  <div className="img-container rounded-full bg-fuchsia-200 w-10 h-10 overflow-hidden">
-                    <Image
-                      height={50}
-                      width={50}
-                      className=""
-                      src="/assets/nellyaran.jpg"
-                      alt=""
-                    />
-                  </div>
-                </div>
-                <div className="av-writeUp ml-5 text-xs font-semibold">
-                  <div className="name mb-1">Nelly Aran</div>
-                  <div className="desc">Candle Forecasts at Inksquare</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="TestimonyTwo">
-            <div className="testi-card bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] p-5 rounded-xl mx-2 my-2">
-              <div className="card-brand mb-4">
-                <div className="icon-container flex items-center font-bold ">
-                  <div className=" mr-2">
                     <FontAwesomeIcon icon={faFacebook} className="w-4 h-4" />
                   </div>
                   <p>Facebook</p>
@@ -250,9 +208,10 @@ export default function Testimonials() {
                       className="w-3 h-3 inline text-gray-300"
                     />
                   </sup>{" "}
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Veniam, deserunt. Voluptate veniam facere, delectus eveniet
-                  nisi quis incidunt mollitia veritatis.{" "}
+                  Trading with this broker has been a game-changer for my
+                  investment portfolio. Their user-friendly platform and
+                  extensive range of financial instruments have allowed me to
+                  diversify my trading strategies effectively{" "}
                   <sup>
                     <FontAwesomeIcon
                       icon={faQuoteRight}
@@ -268,14 +227,63 @@ export default function Testimonials() {
                       height={50}
                       width={50}
                       className=""
-                      src="/assets/nellyaran.jpg"
+                      src="/assets/murat.jpg"
                       alt=""
                     />
                   </div>
                 </div>
                 <div className="av-writeUp ml-5 text-xs font-semibold">
-                  <div className="name mb-1">Da Silva P. Viera</div>
-                  <div className="desc">Indicator Analyst at Voce-Bulls</div>
+                  <div className="name mb-1">Dimitri Paulo</div>
+                  <div className="desc">Account Manager at Greece Chart'Rs</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="TestimonyTwo">
+            <div className="testi-card bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] p-5 rounded-xl mx-2 my-2">
+              <div className="card-brand mb-4">
+                <div className="icon-container flex items-center font-bold ">
+                  <div className=" mr-2">
+                    <FontAwesomeIcon icon={faReddit} className="w-4 h-4" />
+                  </div>
+                  <p>Reddit</p>
+                </div>
+              </div>
+              <div className="card-body text-sm">
+                <blockquote>
+                  <sup>
+                    <FontAwesomeIcon
+                      icon={faQuoteLeft}
+                      className="w-3 h-3 inline text-gray-300"
+                    />
+                  </sup>{" "}
+                  I have been a loyal client of this broker for several years,
+                  and I have never been disappointed. Their professionalism,
+                  reliability, and personalized approach have made them my go-to
+                  choice anytime.{" "}
+                  <sup>
+                    <FontAwesomeIcon
+                      icon={faQuoteRight}
+                      className="w-3 h-3 inline text-gray-300"
+                    />
+                  </sup>
+                </blockquote>
+              </div>
+              <div className="card-author mt-5 flex items-center">
+                <div className="avatar ">
+                  <div className="img-container rounded-full bg-fuchsia-200 w-10 h-10 overflow-hidden">
+                    <Image
+                      height={50}
+                      width={50}
+                      className=""
+                      src="/assets/dumm.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="av-writeUp ml-5 text-xs font-semibold">
+                  <div className="name mb-1">Murat Isik</div>
+                  <div className="desc">Personal Trader</div>
                 </div>
               </div>
             </div>
