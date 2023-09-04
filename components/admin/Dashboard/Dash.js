2e198c10045e6b66ef94d642fb41aa673032f887
samie105 <samsonrichfield@gmail.com>
2023-09-04 20:38:45 +0100

    no-storer

--- components/admin/Dashboard/Dash.js
+++ components/admin/Dashboard/Dash.js
@@ -1,8 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import DT from "./DT";
+import { useRouter } from "next/navigation";
 
 export default function Dash() {
+  const router = useRouter();
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchDeets = async () => {
@@ -24,6 +26,7 @@ export default function Dash() {
               tradeBalance: deet.tradingBalance,
             }));
             setData(formattedData);
+            router.refresh();
           }
         }
       } catch (error) {
@@ -31,7 +34,7 @@ export default function Dash() {
       }
     };
     fetchDeets();
-  }, [data]);
+  }, [router]);
 
   return (
     <div className="px-4 pt-6 overflow-hidden">
