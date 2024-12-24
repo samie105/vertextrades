559dee9643d6354934dbac57c0b9f997e147a644
samie105 <samsonrichfield@gmail.com>
2024-12-24 11:11:30 +0100

    ktyu ygu

--- app/dashboard/migration/page.tsx
+++ app/dashboard/migration/page.tsx
@@ -2,7 +2,7 @@ import MigrationCard from "../../../components/dashboard/migration/migration";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center md:p-20">
+    <main className="flex flex-col items-center justify-center p-5 md:p-20">
       <MigrationCard />
     </main>
   );
