d91c057c38b8766c17f4adb83bbb2b037dc4daf8
samie105 <samsonrichfield@gmail.com>
2025-02-02 10:23:53 +0100

    ghh

--- app/db/updateUser/api/route.js
+++ app/db/updateUser/api/route.js
@@ -18,7 +18,7 @@ const sendVerificationEmail = async (name, email) => {
     subject: "ID Approved",
     html: `
       <p>Dear ${name},</p>
-      <p>We are pleased to inform you that your KYC has been approved. You can now enjoy full access to our platform and its features.</p>
+      <p>We are pleased to inform you that your KYC has been approved and your account is now verified. You can now enjoy full access to our platform and its features.</p>
       <p>If you have any questions or need further assistance, please do not hesitate to contact our support team.</p>
       <p>Best regards,</p>
       <p>Zensync Market Team</p>
